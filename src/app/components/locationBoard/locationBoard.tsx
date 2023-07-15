7af686414deed92dc7e983c30eb117ab877c5dae
Najah7 <najahmatsuo7@gmail.com>
2023-07-16 03:34:49 +0900

    🩹 #32 Headerを分離

--- src/app/components/locationBoard/locationBoard.tsx
+++ src/app/components/locationBoard/locationBoard.tsx
@@ -4,16 +4,16 @@ import Image from 'next/image'
 
 interface LocationBoardProps {
     address: string
-    restaurant_name: string
+    name: string
 }
 
-const LocationBoard: React.FC<LocationBoardProps> = ({ address, restaurant_name }: LocationBoardProps) => {
+const LocationBoard: React.FC<LocationBoardProps> = ({ address, name }: LocationBoardProps) => {
     return (
         <div className={styles.container}>
             <Image className={styles.icon} src="/icon/location.png" alt="location" width={30} height={30} />
             <div className={styles.location}>
                 <h2 className={styles.address}>東京都</h2>
-                <p className={styles.restaurant_name}>A支店</p>
+                <p className={styles.name}>A支店</p>
             </div>
             
         </div>
