7ee5fe72bd0cbbaa108bc28ab87ba7bd73bfa99e
Najah7 <najahmatsuo7@gmail.com>
2023-07-16 11:39:24 +0900

    🩹 ホームページをエンドポイントを「/」に変更（フォームを少し修正）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+'use client'
 import Image from 'next/image'
+import Button from './components/button/button'
+import styles from './home.module.css'
+import { useRouter, NextRouter} from 'next/navigation'
+
 
 export default function Home() {
+  
+  const router: NextRouter = useRouter()
+
   return (
     <main>
       <div>
-        <p>home</p>
+        <div className={styles.main_visual}>
+            <Image src="/tippers-logo.png" alt="tippers" width={320} height={150} />
+            <h2>Try to change Japan</h2>
+            <h2>By giving tips</h2>
+        </div>
+        <div className={styles.buttons}>
+          <Button text="Log in" onClick={()=>router.push('/login')}/>
+          <Button text="Sign up" onClick={()=>router.push('/signup')}/>
+        </div>
       </div>
     </main>
   )
